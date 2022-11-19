4730ab81ffe247eb46b5a8a1df6c600904f5e775
samir fakraoui <fakraouisamir303@gmail.com>
2022-11-19 10:50:58 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  constructor(){
+    super(); // calls the constructor in the react componant 
+    
+    this.state = {
+      monsters:[]
+    }
+  };
+
+  // called when the app get rendered to the dom for the 
+  // first time
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState({monsters : users}))
+  }
+
+  render(){
+    return (
+      <div className="App">
+         <h1 className='text'>Monsters liste</h1>
+         {
+           this.state.monsters.map( monster => {
+            return(
+              <div key={monster.id} className="monster">
+                <h2> {monster.username}</h2>
+                <h3>{monster.email}</h3>
+              </div>
+            )
+           }) 
+         }
+      </div>
+    );
+  };
+}
+
+export default App;
