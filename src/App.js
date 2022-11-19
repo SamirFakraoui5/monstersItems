951d31ab175d786a2765f272a7b853ae4121f7b3
samir fakraoui <fakraouisamir303@gmail.com>
2022-11-19 12:01:07 +0100

    modifier app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { CardList } from './componants/card-list/card.componant';
+import { CardList } from './componants/card-list/card-list.componant';
 
 class App extends React.Component {
   constructor(){
